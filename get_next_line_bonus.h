e9aed102e0b9671851e313bc1ce01e43f80e92e4
Victor <Victor@MacBook-Pro-de-Victor.local>
2019-12-07 23:43:07 +0100

    bonus files added

---
+++ get_next_line_bonus.h
@@ -0,0 +1,15 @@
+#ifndef	GET_NEXT_LINE_H
+#define	GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int	get_next_line(int fd, char **line);
+int     ft_analyse(char *buffer);
+char    *ft_realloc_content(char *line, char *buffer);
+char    *ft_cut_line(char *buffer, char **line);
+void	ft_free_memory(char **p);
+
+#endif
